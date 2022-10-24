13aa24658649242f653a2e5f547086a4c2452827
Vincent Bradbury <57743812+SDRonin@users.noreply.github.com>
2022-10-24 16:52:53 -0500

    Add react logo to background image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,9 @@
 import React from "react"
+import bgLogo from "../react-icon-large.png"
 
 export default function Main() {
     return (
-        <div className="mainContent">
+        <div className="mainContent" style={{background: `no-repeat right 75% url(${bgLogo})` }}>
             <h1 className="mainContentTitle">Fun facts about React</h1>
             <ul className="mainContentList">
                 <li>Was first released in 2013</li>
